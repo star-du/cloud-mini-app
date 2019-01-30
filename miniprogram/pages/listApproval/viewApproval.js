b1a83fd3f61fc232b22427feb4b99eaa6bebeaf3
Dorence Deng <dorencedeng@gmail.com>
2019-01-30 17:16:37 +0800

    后台管理
    一些小改动

--- miniprogram/pages/listApproval/viewApproval.js
+++ miniprogram/pages/listApproval/viewApproval.js
@@ -7,17 +7,34 @@ Page({
   data: {
     icon: base64.icon20
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     const PAGE = this; // 使得get回调函数可以访问this.setData
     // 获取db数据
+    if (!options.id) {
+      wx.showToast({
+        title: "无效访问",
+        icon: "none",
+        mask: true,
+        duration: 3000,
+        complete: function() {
+          setTimeout(function() {
+            wx.navigateBack({
+              delta: 1
+            });
+          }, 3200);
+        }
+      });
+      return;
+    }
     db.collection('forms').where({
-      _id : options.id
+      _id: options.id
     }).get({
       success(e) {
         console.log(e.data);
-        if(e.data&&e.data.length===1){
+        if (e.data && e.data.length === 1) {
           let x = e.data[0];
           x.submitDate = x.submitDate.toLocaleDateString();
+          x.eventDate = x.eventDate ? new Date(x.eventDate).toLocaleDateString() : "";
           PAGE.setData({
             appr: x || {}
           });
@@ -27,53 +44,29 @@ Page({
       fail: console.error
     });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
+  onReady: function() {},
+  onShow: function() {},
+  onHide: function() {},
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
