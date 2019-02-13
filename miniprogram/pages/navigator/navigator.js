078d75274ab1c3fa6fba023db3abb9df45dcd7d1
Kimcurator <1095165823@qq.com>
2019-02-13 21:42:05 +0800

    登录逻辑更新

--- miniprogram/pages/navigator/navigator.js
+++ miniprogram/pages/navigator/navigator.js
@@ -1,4 +1,6 @@
 // miniprogram/pages/navigator/navigator.js
+const app = getApp()
+
 Page({
 
   /**
@@ -13,10 +15,6 @@ Page({
       {
         name:'progressCheck',
         name_zh: '进度查询'
-      },
-      {
-        name:'admin',
-        name_zh: '审批'
       }
     ]
   },
@@ -24,8 +22,17 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function (options) {//如果是管理员，则渲染审批界面**存在bug，用户点击过快时，尚未同步数据库而已开始鉴权，导致管理员用户仍无法渲染出审批**
+    
+    const isAdmin = app.globalData.isAdmin;
+    let enter = this.data.enter;
+    enter.push({
+      name: 'admin',
+      name_zh: '审批'
+    });
+    if(isAdmin){
+      this.setData({enter})
+    }
   },
 
   /**
