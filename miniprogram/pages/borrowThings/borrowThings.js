924df020e466eae9dc4f13db17d4f5a9a2bb9f55
Kimcurator <1095165823@qq.com>
2019-04-13 13:49:00 +0800

    database/UI fixed

--- miniprogram/pages/borrowThings/borrowThings.js
+++ miniprogram/pages/borrowThings/borrowThings.js
@@ -5,103 +5,34 @@ Page({
   data: {
     items: 0,
     tags: ["锦旗", "灯", "电子设备", "杂物"],
-    goods: [{
-        "name": "锦旗",
-        "items": [{
-          "_id": "1",
-          "itemid": 1,
-          "name": "思存工作室锦旗1",
-          "description": "红底黄字的一面锦旗",
-          "count": 1,
-          "tags": ["锦旗"],
-          "repo": 1
-        }, {
-          "_id": "1",
-          "itemid": 1,
-          "name": "思存工作室锦旗2",
-          "description": "红底黄字的一面锦旗",
-          "count": 1,
-          "tags": ["锦旗"],
-          "repo": 1
-        }, {
-          "_id": "1",
-          "itemid": 1,
-          "name": "思存工作室锦旗3",
-          "description": "红底黄字的一面锦旗",
-          "count": 1,
-          "tags": ["锦旗"],
-          "repo": 1
-        }]
-      }, {
-        "name": "灯",
-        "items": [{
-            "_id": "2",
-            "itemid": 1,
-            "name": "led灯1",
-            "description": "用于照明",
-            "count": 2,
-            "tags": ["灯"],
-            "repo": 2
-          },
-          {
-            "_id": "2",
-            "itemid": 1,
-            "name": "led灯2",
-            "description": "用于照明",
-            "count": 2,
-            "tags": ["灯"],
-            "repo": 2
-          }
-        ]
-      },
-      {
-        "name": "电子设备",
-        "items": [{
-          "_id": "3",
-          "itemid": 3,
-          "name": "音响",
-          "description": "不是hifi",
-          "count": 1,
-          "tags": ["电子设备"],
-          "repo": 1
-        }]
-      },
-      {
-        "name": "杂物",
-        "items": [{
-            "_id": "4",
-            "itemid": 4,
-            "name": "就是杂物1",
-            "description": "反正就是杂物",
-            "count": 2,
-            "tags": ["杂物"],
-            "repo": 2
-          },
-          {
-            "_id": "4",
-            "itemid": 4,
-            "name": "就是杂物2",
-            "description": "反正就是杂物",
-            "count": 2,
-            "tags": ["杂物"],
-            "repo": 2
-          }
-        ]
-      }
-    ],
+    goods: [],
     toView: '0',
     scrollTop: 100,
-    foodCounts: 0
+    foodCounts: 0,
+    carArray: [],
+    item:[],
   },
-
-
-
+  //点击加号跳转至表单
+  navtoForm(e) {
+    var index = e.currentTarget.dataset.itemIndex;
+    var parentIndex = e.currentTarget.dataset.parentindex;
+    var name = this.data.goods[parentIndex].items[index].name;
+    var mark = 'a' + index + 'b' + parentIndex;
+    var obj = {name: name, index: index, parentIndex: parentIndex};
+    var carArray1 = this.data.carArray.filter(item => item.mark != mark)
+    carArray1.push(obj)
+    console.log(carArray1);
+    this.setData({
+      carArray: carArray1,
+      goods: this.data.goods,
+      item: this.data.goods[parentIndex].items[index],
+    });
+    const data = e.currentTarget.dataset;
+    wx.navigateTo(data);
+  },
+  
   onLoad: function (options) {
-    let itemArr = this.initObject();
-    // this.setData({
-    //   goods: itemArr
-    // })
-    console.log(this.data)
+    this.getDatabase();
   },
 
   //导航栏跳转
@@ -115,33 +46,13 @@ Page({
 
   //获取数据库某一类物品的数据，并返回一个对象
   //name=>类型，items=>物品列表
-  getDatabase: function (tag) {
-    console.log("getdatabase")
-    db.collection("items").where({
-        tags: [tag]
-      }).get()
-      .then(e => {
-        console.log("data:" + e.data)
-        let filteditem = {
-          "name": tag,
-          "items": e.data
-        };
-        return filteditem;
+  getDatabase: function () {
+    const PAGE = this;
+    db.collection("items").get().then(e => {
+      PAGE.setData({
+        goods:e.data
       })
-      .catch(err => {
-        console.error(err)
-      })
-  },
-
-  //组合对象
-  initObject: function () {
-    let tags = this.tags;
-    let itemArr = []
-    for (let i in tags) {
-      itemArr.push(getDatabase(tags[i]))
-    }
-    console.log(itemArr)
-    return itemArr;
+    })
   },
 
   onReady: function () {
